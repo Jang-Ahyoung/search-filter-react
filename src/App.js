83c69b7863d44d071bb239c700732fe4393825eb
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-01-31 14:40:17 +0900

    🏍 making sarch bar filter with random data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import JSONDATA from './MOCK_DATA.json';
+import { useState } from 'react';
 
 function App() {
+  const [searchword, setSearchword] = useState('')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="InputBox">
+        <input type="text" placeholder="Search. . ." onChange={event => { setSearchword(event.target.value) }} />
+        {console.log(searchword)}
+
+      </div>
+      {JSONDATA.filter((val) => {
+        if (searchword == '') return val;
+        else if (val.first_name.toLowerCase().includes(searchword.toLowerCase())) {
+          return val;
+        }
+      }).map((data, key) => {
+        return (
+          <div key={key}>
+            <p>{data.first_name}</p></div>
+        )
+      })}
     </div>
   );
 }
